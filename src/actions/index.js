51ac8372bbeeecddaad174da2c8400cda721cf1b
CharDale Barnes <nitinolmind@CharDales-MacBook-Pro.local>
2019-04-24 22:53:02 -0500

    Final weatherAPI implementation. Covers practice using reduxpromise, avoiding state mutation, and using 3rd party applications and apis

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f86c6461ea8ee41c61e58fd3da0a128b';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 // action creator
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+// import _ from 'lodash';
+import GoogleMap from '../components/googleMap';
 
 class WeatherList extends Component {
     renderWeather( cityData ) {
         const name = cityData.city.name;
         // get an array of temperatures:
         const temps = cityData.list.map(weather => weather.main.temp);
+        // ^^ original, but if we want to convert to farenheit:
+        // const temps = _.map(cityData.list.map(weather => weather.main.temp), temp => temp / conversionhere);
         const pressure = cityData.list.map(baro => baro.main.pressure);
         const humid = cityData.list.map(humidity => humidity.main.humidity);
+        // const lon = cityData.city.coord.lon;
+        // const lat = cityData.city.coord.lat;
+        // es6: instead:
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={name}>
-                <td>
-                    {name}
+                <td>{name}
+                {/* component name */}
+                <GoogleMap lat={lat} lon={lon} />
                 </td>
-                <td>
-                    <Chart data={temps} color="orange" />
-                </td>
-                
+                <td><Chart data={temps} color="orange" units={"\u00b0F"}/></td>
+                <td><Chart data={pressure} color="blue" /></td>
+                <td><Chart data={humid} color="black" /></td>   
             </tr>
         )
     }
